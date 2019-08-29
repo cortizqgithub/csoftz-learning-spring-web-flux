a91e3558d614171b3ca7d6a0c0f9bfe36888ab3f
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2019-08-28 23:22:55 -0500

    Adds required files for 'Stock Quotes' Application.

---
+++ app-code/back-end/microservices/stock-quotes/src/main/java/io/spring/workshop/stockquotes/domain/Quote.java
@@ -0,0 +1,31 @@
+package io.spring.workshop.stockquotes.domain;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.time.Instant;
+
+import lombok.Data;
+
+@Data
+public class Quote {
+    private static final MathContext MATH_CONTEXT = new MathContext(2);
+    private String ticker;
+    private BigDecimal price;
+    private Instant instant = Instant.now();
+
+    public Quote() {
+    }
+
+    public Quote(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public Quote(String ticker, BigDecimal price) {
+        this.ticker = ticker;
+        this.price = price;
+    }
+
+    public Quote(String ticker, Double price) {
+        this(ticker, new BigDecimal(price, MATH_CONTEXT));
+    }
+}
